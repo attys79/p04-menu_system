cedd5d13314b0e08ae3e5e676f445e431383ecb6
attys79 <75606949+attys79@users.noreply.github.com>
2020-12-09 13:08:38 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,42 @@
+basic.showLeds(`
+    # . # . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    `)
+let item = 0
+let counter = 0
 basic.forever(function () {
-	
+    led.unplot(2, item)
+    if (input.buttonIsPressed(Button.A)) {
+        item += 1
+        if (item == 5) {
+            item = 0
+        }
+        while (input.buttonIsPressed(Button.A)) {
+            basic.pause(1)
+        }
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showNumber(item)
+        while (input.buttonIsPressed(Button.B)) {
+            basic.pause(1)
+        }
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            `)
+    }
+    counter += 1
+    if (counter == 20) {
+        counter = 0
+    }
+    if (counter < 10) {
+        led.plot(2, item)
+    }
+    basic.pause(10)
 })
